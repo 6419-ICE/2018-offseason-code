097a5782699e37b9fffb8988040d4ecfa41ba508
Cobeasta <stopyloknok@gmail.com>
2018-07-25 19:55:26 -0500

    Trying to fix the broken git

--- src/org/usfirst/frc/team6419/robot/Robot.java
+++ src/org/usfirst/frc/team6419/robot/Robot.java
@@ -160,7 +160,6 @@ public class Robot extends TimedRobot {
 	@Override
 
 	public void autonomousInit() {
-
 	//	m_autonomousCommand = m_chooser.getSelected();
 
 		m_autonomousCommand = new DriveToPoint(10, 0);
